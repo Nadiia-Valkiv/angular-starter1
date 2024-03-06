e208637437edf21aee01334c849fc0fe79a473a6
Nadiia-Valkiv <nadiia.mykhalevych7@gmail.com>
2024-03-06 18:00:58 +0200

    contact panel

---
+++ src/app/modules/home/pages/home/anti-corruption-contacts/anti-corruption-contacts.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-anti-corruption-contacts',
+  templateUrl: './anti-corruption-contacts.component.html',
+  styleUrls: ['./anti-corruption-contacts.component.scss']
+})
+export class AntiCorruptionContactsComponent {
+
+}
